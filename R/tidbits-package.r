b4b31cf47e4353ccbb54410df5ae23ff3094590c
Alex Bokov <bokov@uthscsa.edu>
2019-09-13 20:08:59 -0500

    multi;docs;Several new Rd files including a package level help file, and a placeholder vignette.

---
+++ R/tidbits-package.r
@@ -0,0 +1,10 @@
+#' tidbits.
+#'
+#' Various functions to streamline tasks that are repetitive and/or require
+#' funky code tricks. A byproduct of UTHealth's TSCI 5050 Introduction to Data
+#' Science course.
+#'
+#' @name tidbits
+#' @docType package
+#' @importFrom methods is
+NULL
