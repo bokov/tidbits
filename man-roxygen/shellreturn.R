a302e0b2e61ab365e6a525bd6b0d70d81c1d10af
Alex Bokov <bokov@uthscsa.edu>
2019-10-16 22:44:58 -0500

    man-roxygen; doc; Added a roxygen template

---
+++ man-roxygen/shellreturn.R
@@ -0,0 +1,4 @@
+#' @return 0 for successful execution unless \code{intern=TRUE} is part of \code{...} and if it is,
+#'         output from the shell will be returned rather than printed to the
+#'         screen for most commands.
+
