c602f749d04a7a5a1afb87cd848b5a224142a53b
masochist <masochist@loopback.org>
2019-10-17 12:42:39 -0500

    test_instrequire; testing; Added more coverage for 'instrequire()'

--- tests/testthat/test_instrequire.R
+++ tests/testthat/test_instrequire.R
@@ -1,9 +1,14 @@
 context("Auto install/load libraries with instrequire");
 
-test_that("instrequire"
-          ,{
-            expect_null(instrequire(c('readr','tibble','dplyr')))
-            expect_equal(length(intersect(search()
-                                          ,c('package:readr','package:tibble'
-                                             ,'package:dplyr'))),3)
-          })
+test_that("instrequire",{
+  # list of libraries, including a previously not installed one are processed
+  expect_null(instrequire(c('extrafontdb','readr','readxl','tibble')));
+  # now they should all show up as loaded
+  expect_length(intersect(paste0('package:',c('extrafontdb','readr','readxl'
+                                              ,'tibble')),search()),4);
+  expect_error(instrequire(c('NONEXISTENT','readr','readxl')));
+});
+
+# cleanup
+remove.packages('extrafontdb',.libPaths());
+c()
