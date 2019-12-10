04a7a9e858dfd903cbe196ae6b6294c92d575eb9
Alex Bokov <bokov@uthscsa.edu>
2019-12-10 14:01:26 -0600

    functions.R; deployability; Added 'type','checksource', and 'compile' variables to 'instrequire()' and set the defaults such that it will never compile source packages on MacOS and Windows if binary packages are available, even if they are out of date

--- R/functions.R
+++ R/functions.R
@@ -17,6 +17,13 @@ magrittr::`%>%`
 #' @param quietly      passed to \code{\link[base]{require}}
 #' @param dependencies whether to also install dependencies, passed to
 #'                     \code{\link[utils]{install.packages}}
+#' @param type         passed to \code{link[utils]{install.packages}}
+#' @param checksource  temporarily sets
+#'                     \code{options('install.packages.check.source')}
+#'                     to this value
+#' @param compile      temporarily sets
+#'                     \code{options('install.packages.compile.from.source')}
+#'                     to this value
 #' @param repos        passed to \code{\link[utils]{install.packages}} to
 #'                     specify which repositories to use (optional).
 #' @param ...          passed to \code{\link[utils]{install.packages}} to
@@ -27,14 +34,26 @@ magrittr::`%>%`
 #' @export
 instrequire <- function(pkgs # nodeps
                         ,quietly=TRUE
-                        # the dependencies argument is ignored and is only here
-                        # so that it doesn't end up in the '...'
+                        # the dependencies argument is ignored and is
+                        # only here so that it doesn't end up in '...'
                         ,dependencies=TRUE
+                        ,type='binary'
+                        ,checksource='no'
+                        ,compile='never'
                         ,repos=getOption('repos','https://cran.rstudio.com/')
                         ,...){
+  orig.checksource <- options('install.packages.check.source');
+  orig.compile <- options('install.packages.compile.from.source');
+  on.exit({
+    options(install.packages.check.source=orig.checksource);
+    options(install.packages.compile.from.source=orig.compile);
+  });
+  options(install.packages.check.source=checksource);
+  options(install.packages.compile.from.source=compile);
   pkgs_installed <- sapply(pkgs,require,character.only=TRUE);
   if(length(pkgs_needed <- names(pkgs_installed[!pkgs_installed]))>0){
-    utils::install.packages(pkgs_needed,repos=repos,dependencies = TRUE,...);
+    utils::install.packages(pkgs_needed,repos=repos,dependencies = TRUE
+                            ,type=type,...);
     pkgs_final <- sapply(pkgs_needed,require,character.only=TRUE
                          ,quietly=quietly);
     if(!all(pkgs_final)){
