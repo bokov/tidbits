282a31ea2b7f3e48752f595e0195b4847f1508df
Alex Bokov <bokov@uthscsa.edu>
2020-01-06 12:22:12 -0600

    R/functions.R,man/instrequire.Rd; tweak; Now the default 'type' argument is 'binary' only for MacOS & Windows. For everything else it's 'source'. Also, suppressing warnings the first time that 'require()' is run.

--- R/functions.R
+++ R/functions.R
@@ -12,12 +12,19 @@ magrittr::`%>%`
 #' attempts to load it. Will produce an error if any packages in \code{pkgs}
 #' are not possible to install and load.
 #'
+#' The goal is to provide a robust and automation-friendly alternative to
+#' putting a bunch of \code{\link[base]{require}} or \code{\link[base]{library}}
+#' statements at the start of each script and have them keep erroring until each
+#' dependency is manually installed.
+#'
 #' @param pkgs         character vector of package names that are needed, e.g.
 #'                     by a script that uses this command.
 #' @param quietly      passed to \code{\link[base]{require}}
 #' @param dependencies whether to also install dependencies, passed to
 #'                     \code{\link[utils]{install.packages}}
-#' @param type         passed to \code{link[utils]{install.packages}}
+#' @param type         passed to \code{link[utils]{install.packages}}. By
+#'                     default it is \code{'binary'} for all MacOS and Windows
+#'                     and \code{'source'} otherwise
 #' @param checksource  temporarily sets
 #'                     \code{options('install.packages.check.source')}
 #'                     to this value
@@ -37,7 +44,8 @@ instrequire <- function(pkgs # nodeps
                         # the dependencies argument is ignored and is
                         # only here so that it doesn't end up in '...'
                         ,dependencies=TRUE
-                        ,type='binary'
+                        ,type=if(get_os() %in% c('osx','windows')){
+                          'binary'} else 'source'
                         ,checksource='no'
                         ,compile='never'
                         ,repos=getOption('repos','https://cran.rstudio.com/')
@@ -50,7 +58,8 @@ instrequire <- function(pkgs # nodeps
   });
   options(install.packages.check.source=checksource);
   options(install.packages.compile.from.source=compile);
-  pkgs_installed <- sapply(pkgs,require,character.only=TRUE);
+  suppressWarnings(pkgs_installed <- sapply(pkgs,require,character.only=TRUE
+                                            ,quietly=quietly));
   if(length(pkgs_needed <- names(pkgs_installed[!pkgs_installed]))>0){
     utils::install.packages(pkgs_needed,repos=repos,dependencies = TRUE
                             ,type=type,...);
@@ -241,7 +250,7 @@ get_os <- function(){ # nodeps
   } else { ## mystery machine
     os <- .Platform$OS.type;
     if (grepl("^darwin", R.version$os)) os <- "osx";
-    if (grepl("linux-gnu", R.version$os)) os <- "linux";
+    if (grepl("linux", R.version$os)) os <- "linux";
   }
   tolower(os);
 };
